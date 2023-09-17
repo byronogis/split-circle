2d772f6dd8d6b410a6e3a0217be420245ea81897
Byron <byronogis@outlook.com>
2023-09-17 17:48:21 +0800

    fix: deplay path

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pkg from '../core/package.json'
 
 export default defineConfig({
+  base: `/${pkg.name}/`,
   plugins: [vue()],
 })
